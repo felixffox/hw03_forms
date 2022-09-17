18b84140d52949a3c43410c0c381e68dad282323
Danil Vdovin <davdovin32@gmail.com>
2022-09-17 23:11:55 +0300

    Первый коммит по 4 спринту

---
+++ yatube/posts/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('text', 'group')
+        labels = {
+            'text': 'Текст поста',
+            'group': 'Группа'
+        }
+
+    def clean_text(self):
+        data = self.cleaned_data['text']
+        if data == '':
+            raise forms.ValidationError('Заполните текст поста')
+        return data
---
+++ yatube/posts/views.py
@@ -0,0 +1,86 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+from .forms import PostForm
+
+from .models import Post, Group
+
+
+def index(request):
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'group': group,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/group_list.html', context)
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    post_list = author.posts.all().order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    paje_obj = paginator.get_page(page_number)
+    context = {
+        'author': author,
+        'page_obj': paje_obj,
+        'post_list': post_list,
+    }
+    return render(request, 'posts/profile.html', context)
+
+def post_detail(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    posts_count = Post.objects.filter(author=post.author).count()
+    context = {
+        'post': post,
+        'posts_count': posts_count,
+    }
+    return render(request, 'posts/post_detail.html', context)
+
+@login_required
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.author = request.user
+            new_post.save()
+            return redirect('posts:profile', request.user)
+        return render(request, 'posts/create_post.html', {'form': form})
+    form = PostForm()
+    return render(request, 'posts/create_post.html', {'form': form})
+
+@login_required
+def post_edit(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    form = PostForm(request.POST, instance=post)
+    is_edit = True
+    context = {
+        'post': post,
+        'form': form,
+        'is_edit': is_edit,
+    }
+    if request.user != post.author:
+        return redirect('posts:post_detail', post_id=post_id)
+    form = PostForm(request.POST, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('posts:post_detail', post_id=post_id)
+    form = PostForm()
+    return render(request, 'posts/create_post.html', context)
